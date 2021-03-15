ebb66553061f0f95c1aec1fdd92914b74d777cea
markoptionb <optionbsupport@gmail.com>
2021-03-15 23:20:37 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+
+// content = someTextArea.value
+// Node.innerHTML = `${newHeader}YOUR-NAME is the champion`
+
+
+
